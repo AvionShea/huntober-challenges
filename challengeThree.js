c9acbf7ef267734be52bebe826514c8c9b0a7bfe
AvionShea <avionsheadesigns@gmail.com>
2024-10-15 18:33:09 -0400

    daily code challenge - refactor

--- challengeThree.js
+++ challengeThree.js
@@ -20,6 +20,9 @@ function sumMix(arr) {
     return arr.reduce((acc, currentValue) => acc + +currentValue, 0);
 };
 
+//refactor
+const sumMix = arr => arr.reduce((a, c) => a + +c, 0)
+
 console.log(sumMix([9, 3, '7', '3']), 22);
 console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42);
 console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2, '0']), 41);
