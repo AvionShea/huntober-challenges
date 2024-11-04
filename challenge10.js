54b342f8e3abe6ef15910d3a7b1d19ed8f89ce3b
AvionShea <avionsheadesigns@gmail.com>
2024-11-04 18:09:11 -0500

    daily code challenge

--- challenge10.js
+++ challenge10.js
@@ -5,4 +5,29 @@ https://medium.com/siliconwat/algorithms-in-javascript-b0bed68f4038
 
 Given an array of numbers, return all pairs that add up to a given sum. The numbers can be used more than once.
 
+arr -> no funny biz
+give me a sum to obtain
+return pairs that = sum
 */
+
+function twoSum(arr, sum) {
+    //keep track of pairs (paris arr)
+    const pairs = [];
+
+    //hashmap obj
+    const seenNums = {};
+
+    //loop
+    for (const currentNum of arr) {
+        const diff = sum - currentNum;
+
+        if (seenNums[diff]) {
+            pairs.push([currentNum, diff])
+        } else {
+            seenNums[currentNum] = true
+        };
+    };
+    return pairs
+};
+
+console.log(twoSum([1, 2, 2, 3, 4], 4), "[[2,2], [3,1]]");
