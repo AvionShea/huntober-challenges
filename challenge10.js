7aebb745caae6c6b1f3545025daf7fae087f1f70
AvionShea <avionsheadesigns@gmail.com>
2024-11-04 19:23:17 -0500

    daily code challenge - updated link

--- challenge10.js
+++ challenge10.js
@@ -1,7 +1,7 @@
 /*
 November 4, 2024
 
-https://medium.com/siliconwat/algorithms-in-javascript-b0bed68f4038
+https://freedium.cfd/https://medium.com/siliconwat/algorithms-in-javascript-b0bed68f4038
 
 Given an array of numbers, return all pairs that add up to a given sum. The numbers can be used more than once.
 
